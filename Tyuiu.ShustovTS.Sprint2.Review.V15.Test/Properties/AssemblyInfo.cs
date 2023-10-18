a99025899387729f0608a8b0e5fe75de03de5d13
Шустов Тима <tima_shustov005@mail.ru>
2023-10-18 16:04:41 +0500

    Review

---
+++ Tyuiu.ShustovTS.Sprint2.Review.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShustovTS.Sprint2.Review.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShustovTS.Sprint2.Review.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f99495c5-2f77-4a46-bd2d-3ef30db5cb02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShustovTS.Sprint2.Review.V15/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShustovTS.Sprint2.Task7.V15.Lib;
+
+
+namespace Tyuiu.ShustovTS.Sprint2.Task7.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Шустов Т.С. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры C#                            *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила:Шустов Тимофей Сергеевыич|АСОиУБ-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с               *");
+            Console.WriteLine("* координатами X,Y в заштрихованной области.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение переменной x  ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение переменной y  ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
